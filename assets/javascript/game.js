1f039e1b758854690e4a43e9bcbfec06e8f59e28
Bijan <bijansemnani@Bijans-MacBook-Pro.local>
2018-02-15 18:22:52 -0800

    initial files

---
+++ assets/javascript/game.js
@@ -0,0 +1,10 @@
+
+$(document).ready(function() {
+  var characters = [Luke = {health: 100, attack: 4, counter: 50},
+                  Palpatine = {health: 200, attack: 6, counter: 70},
+                  ObiWan = {health: 250, attack: 5, counter: 80}];
+
+  console.log(characters);
+  
+
+});
