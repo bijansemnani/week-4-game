539bc25a25f4f131dfe1791268687b03d077d5cf
Bijan <bijansemnani@Bijans-MacBook-Pro.local>
2018-02-18 11:18:42 -0800

    made it so that the player can fight enemies

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,30 +1,39 @@
 
 $(document).ready(function() {
   var chosen;
-  var characters =
-  [Luke = {
-    name: "Luke",
-    health: 100,
-    attack: 4,
-    counter: 50,
-    image: "https://lumiere-a.akamaihd.net/v1/images/luke-skywalker-main_5a38c454_461eebf5.jpeg?region=0%2C0%2C1536%2C864&width=768"
-  },
-   Palpatine = {
-     name: "Palpatine",
-     health: 200,
-     attack: 6,
-     counter: 70,
-     image:"https://vignette.wikia.nocookie.net/starwars/images/9/9a/Palp_trustme.jpg/revision/latest/scale-to-width-down/250?cb=20070114040526"
-  },
-   ObiWan = {
-     name: "ObiWan",
-     health: 250,
-     attack: 5,
-     counter: 80,
-     image:"https://imagesvc.timeincapp.com/v3/fan/image?url=https%3A%2F%2Fwinteriscoming.net%2Ffiles%2F2018%2F02%2FEwan-McGregor-as-Obi-Wan-Kenobi.jpg&c=sc&w=850&h=560"
-  }];
+  var isChosen = false;
+  var opponentChar;
+  var characters =[];
+
+  function resetArray() {
+
+    characters =
+    [Luke = {
+      name: "Luke",
+      health: 400,
+      attack: 4,
+      counter: 50,
+      image: "https://lumiere-a.akamaihd.net/v1/images/luke-skywalker-main_5a38c454_461eebf5.jpeg?region=0%2C0%2C1536%2C864&width=768"
+    },
+     Palpatine = {
+       name: "Palpatine",
+       health: 200,
+       attack: 6,
+       counter: 70,
+       image:"https://vignette.wikia.nocookie.net/starwars/images/9/9a/Palp_trustme.jpg/revision/latest/scale-to-width-down/250?cb=20070114040526"
+    },
+     ObiWan = {
+       name: "ObiWan",
+       health: 250,
+       attack: 5,
+       counter: 80,
+       image:"https://imagesvc.timeincapp.com/v3/fan/image?url=https%3A%2F%2Fwinteriscoming.net%2Ffiles%2F2018%2F02%2FEwan-McGregor-as-Obi-Wan-Kenobi.jpg&c=sc&w=850&h=560"
+    }];
+
+  }
 
   //initial characters array
+  resetArray();
   chooseChars("#images");
 
   function chooseChars(string) {
@@ -36,21 +45,52 @@ $(document).ready(function() {
       newDiv.data("data-object", characters[i]);
       $(string).append(newDiv);
     }
+    if(isChosen === true){
+      $("#images").html("<h2>Your Character: </h2>")
+      $("#images").append("<img src="+ chosen.image + " height='100'>");
+      $("#images").append("<button id='attack' class='attackClass'>Attack</button>");
+      isChosen === false;
+    }
   }
+
+  $("#images").on("click","button.attackClass", function () {
+    opponentChar.health -= chosen.attack;
+    chosen.health -= opponentChar.counter;
+    chosen.attack += chosen.attack;
+    console.log("chosen health: " + chosen.health);
+    console.log("chosen attack: " + chosen.attack);
+    console.log("opponent health: " + opponentChar.health);
+    if(chosen.health <= 0){
+      $("#images").html("You Lose");
+    }
+    if(opponentChar.health <= 0){
+      $("#images").html("You defeated" + opponentChar.name + "choose a new opponent");
+    }
+  });
 
   //save the chosen character to be used by user
   $("#images").on("click","button.buttonId", function () {
     chosen = $(this).data("data-object");
+    isChosen = true;
     $("#images").empty();
     chooseChars("#opponent");
     console.log(chosen);
+  });
+
+  $("#opponent").on("click","button.buttonId", function () {
+    opponentChar = $(this).data("data-object");
+    $("#opponent").html("<h2>Your opponent:</h2>");
+    $("#opponent").append("<img src="+ opponentChar.image + " height='100'>");
+    console.log(opponentChar);
   });
 
   //reset the game
   $("#reset").on("click", function () {
     $("#images").empty();
     $("#opponent").empty();
-    console.clear()
+    resetArray();
+    isChosen = false;
+    console.clear();
     chosen = "";
     chooseChars("#images");
 
