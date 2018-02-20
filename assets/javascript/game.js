f94ad1ecb367a9ab8d5bb2dd0477ef4e9d6471d2
Bijan <bijansemnani@Bijans-MacBook-Pro.local>
2018-02-20 12:20:56 -0800

    organized the code and made it look pretty

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -59,14 +59,12 @@ $(document).ready(function() {
   resetArray();
   chooseChars("#images");
 
+  //function to print opp and user char options list
   function chooseChars(string) {
     $(string).html("<h2>Pick your Character:</h2>")
     for (var i = 0; i < characters.length; i++) {
-      console.log(i);
       if(chosen === characters[i]){
-        console.log("here" + i);
         characters.splice(i,1);
-        console.log(characters);
         if(i > characters.length - 1)
           continue;
       }
@@ -76,33 +74,46 @@ $(document).ready(function() {
       newDiv.data("data-object", characters[i]);
       $(string).append(newDiv);
     }
-    if(isChosen === true){
-      yourChar();
-    }
   }
 
-  function yourChar() {
-    $("#images").html("<h2>Your Character: </h2>")
-    $("#images").append("<button class='pictures'><img src="
-    + chosen.image + " height='100'><div>" +
-    chosen.health + "</div></img></button>");
-    $("#images").append("<button id='attack' class='attackClass'>Attack</button>");
+  //save the chosen character to be used by user then print chosen char
+  $("#images").on("click","button.buttonId", function () {
+    chosen = $(this).data("data-object");
+    isChosen = true;
+    $("#images").empty();
+    chooseChars("#opponent");
+    if(isChosen === true)
+      Chars("#images",chosen);
+  });
+
+  //save the opponent character then print opp char
+  $("#opponent").on("click","button.buttonId", function () {
+    opponentChar = $(this).data("data-object");
+    for (var i = 0; i < characters.length; i++) {
+      if(opponentChar === characters[i])
+        characters.splice(i,1);
+    }
+    Chars("#opponent",opponentChar);
+  });
+
+  //generic print function to print the user and opp chars
+  function Chars(tag,char) {
+    $(tag).html("<h2>Your Character: </h2>")
+    $(tag).append("<button class='pictures'><img src="
+    + char.image + " height='100'><div>" +
+    char.health + "</div></img></button>");
+    if(tag === "#images")
+      $("#images").append("<button id='attack' class='attackClass'>Attack</button>");
     isChosen === false;
   }
 
-  function oppChar() {
-    $("#opponent").html("<h2>Opponent Character: </h2>")
-    $("#opponent").append("<button class='pictures'><img src="
-    + opponentChar.image + " height='100'><div>" +
-    opponentChar.health + "</div></img></button>");
-  }
-
+  //When user attacks the healths and user attacks are modified
   $("#images").on("click","button.attackClass", function () {
     opponentChar.health -= chosen.attack;
     chosen.health -= opponentChar.counter;
     chosen.attack += chosen.attack;
-    yourChar();
-    oppChar();
+    Chars("#images",chosen);
+    Chars("#opponent",opponentChar);
     $("#stat").html("  chosen health: " + chosen.health);
     $("#stat").append("<br>  chosen attack: " + chosen.attack);
     $("#stat").append(" <br> opponent health: " + opponentChar.health);
@@ -111,29 +122,10 @@ $(document).ready(function() {
     }
     else if(opponentChar.health <= 0){
       $("#stat").append("<br> You defeated " + opponentChar.name + " choose a new opponent");
-      if(characters.length === 0){
+      if(characters.length === 0)
         $("#opponent").html("<h1> YOU WIN!!!</h1>");
-      }
       chooseChars("#opponent")
     }
-  });
-
-  //save the chosen character to be used by user
-  $("#images").on("click","button.buttonId", function () {
-    chosen = $(this).data("data-object");
-    isChosen = true;
-    $("#images").empty();
-    chooseChars("#opponent");
-  });
-
-  $("#opponent").on("click","button.buttonId", function () {
-    opponentChar = $(this).data("data-object");
-    for (var i = 0; i < characters.length; i++) {
-      if(opponentChar === characters[i]){
-        characters.splice(i,1);
-      }
-    }
-    oppChar();
   });
 
   //reset the game
